3c36acd19828c92e22369ac5787137179a13594b
nyyxxx <40508636+nyyxxx@users.noreply.github.com>
2021-04-19 13:19:05 +0800

    Rename CEB/model.py to CEB+EIB/model.py

--- CEB+EIB/model.py
+++ CEB+EIB/model.py
@@ -19,14 +19,14 @@ class ToyNet(nn.Module):
             nn.ReLU(True),
             nn.Linear(1024, 1024),
             nn.ReLU(True),
-            nn.Linear(1024, 2*self.K))#¼òµ¥ÍøÂç x-z µÃz(mu,std)
+            nn.Linear(1024, 2*self.K))#ç®€å•ç½‘ç»œ x-z å¾—z(mu,std)
 
         self.decode = nn.Sequential(
-                nn.Linear(self.K, 10))#Ò»²ãÏßÐÔ  z-y
+                nn.Linear(self.K, 10))#ä¸€å±‚çº¿æ€§  z-y
         self.bwencode = nn.Sequential(
-            nn.Linear(10, 2*self.K))# Ò»²ãÏßÐÔbackwards encoder z-y  !!!
-        #Ä£ÐÍ¿É¸Ä¶¯Ö®´¦£ºK,bw_encoder¸Ä³É¶à²ã£¬loss ¸Ä³Éconsistent classifier?!!
-        #ÑéÖ¤bw_encoderµÃµ½µÄz~ºÍzºÜ½Ó½ü?!!
+            nn.Linear(10, 2*self.K))# ä¸€å±‚çº¿æ€§backwards encoder z-y  !!!
+        #æ¨¡åž‹å¯æ”¹åŠ¨ä¹‹å¤„ï¼šK,bw_encoderæ”¹æˆå¤šå±‚ï¼Œloss æ”¹æˆconsistent classifier?!!
+        #éªŒè¯bw_encoderå¾—åˆ°çš„z~å’Œzå¾ˆæŽ¥è¿‘?!!
 
     def forward(self, x, num_sample=1):
         if x.dim() > 2 : x = x.view(x.size(0),-1)
@@ -45,7 +45,7 @@ class ToyNet(nn.Module):
 
         if num_sample == 1 : pass
         elif num_sample > 1 : logit = F.softmax(logit, dim=2).mean(0)
-        #consistent classifier£ºlogit=F.softmax(logit, dim=2).mean(0)
+        #consistent classifierï¼šlogit=F.softmax(logit, dim=2).mean(0)
 
 
         return (mu, std), logit,(mu2, std2)#!!!
